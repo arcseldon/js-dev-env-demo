6f59e49be6a6a225afde8a4efd014a70c904158f
Richard Seldon <arcseldon@gmail.com>
2016-11-20 23:30:40 +0900

    updates for prd

---
+++ src/vendor.js
@@ -0,0 +1,7 @@
+/* this file contains references to the vendor libraries we are using in this project
+   used by webpack in the production build only
+*/
+
+/* eslint-disable no-unused-vars */
+
+import fetch from 'whatwg-fetch';
